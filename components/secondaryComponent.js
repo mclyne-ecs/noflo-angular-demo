80ed29e5e134152d31f4e16efb645738a2ca3eb9
Clyne, Michael <michael.clyne@fmr.com>
2019-04-03 10:44:56 -0400

    adding secondary component

---
+++ components/secondaryComponent.js
@@ -0,0 +1,51 @@
+const noflo = require('noflo');
+
+exports.getComponent = () => {
+  const c = new noflo.Component();
+
+  c.description = 'check input length';
+  c.icon = 'cog';
+
+  c.inPorts.add(
+    'in',
+    { datatype: 'boolean'}
+  );
+  c.inPorts.add(
+    'testcomp_in',
+    { datatype: 'int'}
+  );
+  c.outPorts.add(
+    'success',
+    { datatype: 'int'}
+  );
+  c.outPorts.add(
+    'failure',
+    { datatype: 'string'}
+  );
+  c.outPorts.add(
+      'error',
+      { datatype: 'object'}
+  );
+
+  c.process((input, output) => {
+    // If there is no data then return
+    if(!input.hasData('in')) {
+      return
+    }
+
+    const secondaryCheck = input.getData('in');
+    const testComponentValue = input.getData('testcomp_in');
+    // Only send the value forward if some logical conditon is met
+    if (secondaryCheck) {
+      output.send({
+        out: testComponentValue
+      });
+    } else {
+      output.send({
+        out: 'Failure'
+      });
+    }
+    output.done();
+  });
+  return c;
+};
