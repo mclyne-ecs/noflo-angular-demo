88319bda1282597623f1664eaa7caed5eed099e0
Clyne, Michael <michael.clyne@fmr.com>
2019-04-22 11:56:30 -0400

    adding primary login compo

---
+++ components/primaryLoginComponent.js
@@ -0,0 +1,37 @@
+const noflo = require('noflo');
+
+exports.getComponent = () => {
+  const c = new noflo.Component();
+
+  c.description = 'primary login';
+  c.icon = 'cog';
+
+  c.inPorts.add(
+    'in',
+    { datatype: 'object'}
+  );
+  c.outPorts.add(
+    'out',
+    { datatype: 'object'}
+  );
+  c.outPorts.add(
+      'error',
+      { datatype: 'object'}
+  );
+
+  c.process((input, output) => {
+    // If there is no data then return
+    if(!input.hasData('in')) {
+      return
+    }
+
+    const input = input.getData('in');
+    console.log(input.username)
+    console.log(input.password)
+    output.send({
+      out: input
+    });
+    output.done();
+  });
+  return c;
+};
