5b525d005643b116a36b49b3fa7815551f055310
Clyne, Michael <michael.clyne@fmr.com>
2019-04-23 10:58:28 -0400

    adding changes to secondary login comp

--- components/secondaryLoginComponent.js
+++ components/secondaryLoginComponent.js
@@ -2,28 +2,65 @@ const noflo = require('noflo');
 
 exports.getComponent = () => {
   const c = new noflo.Component();
+
   c.description = 'Secondary login';
   c.icon = 'cog';
-  c.inPorts.add('in', {
-    datatype: 'all',
-    description: 'Packet to forward'
-  });
-  c.outPorts.add('out', {
-    datatype: 'all'
-  });
-  c.process((input, output) => {
-    // Check preconditions on input data
-    if (!input.hasData('in')) {
-      return;
+
+  c.inPorts.add(
+    'secondary_in',
+    { datatype: 'string'}
+  );
+  c.inPorts.add(
+    'decision_in',
+    { datatype: 'string'}
+  );
+  c.outPorts.add(
+    'success',
+    { datatype: 'string'}
+  );
+  c.outPorts.add(
+    'failure',
+    { datatype: 'string'}
+  );
+  c.outPorts.add(
+      'error',
+      { datatype: 'object'}
+  );
+
+  c.autoOrdering = false;
+
+  // Variables to hold values from previous components
+  let decisionResult;
+  let secondaryLogin;
+  c.process((input, output, context) => {
+    if (input.hasData('decision_in')) {
+      decisionResult = input.getData('decision_in');
+      console.log('Decision Result: ' + decisionResult);
+      context.activate();
     }
-    // Read packets we need to process
-    const data = input.getData('in');
-    // Process data and send output
-    output.send({
-      out: data
-    });
-    // Deactivate
-    output.done();
+
+    if (input.hasData('secondary_in')) {
+      secondaryLogin = input.getData('secondary_in');
+      console.log('Secondary Login: ' + secondaryLogin);
+
+      if (secondaryLogin === 'yes') {
+        output.send({
+          success: decisionResult + '. Second Factor Login Complete!'
+        });
+      } else {
+        output.send({
+          failure: 'Failure'
+        });
+      }
+      output.done();
+    }
   });
+
+  c.tearDown = (callback) => {
+    c.emit('end');
+    c.started = false;
+    callback();
+  }
+
   return c;
 };
